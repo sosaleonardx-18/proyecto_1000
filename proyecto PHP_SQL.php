2e4d84d82e238579032e2cd72b0a144967c54bbd
sosaleonardx-18 <118859300+sosaleonardx-18@users.noreply.github.com>
2022-11-22 21:20:42 -0300

    Add files via upload

---
+++ proyecto PHP_SQL.php
@@ -0,0 +1,21 @@
+<!--pagina principal...-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PROYECTO PHP_SQL</title>
+    <link rel="stylesheet" href="proyecto_PHP_SQL.css" type="text/css">
+</head>
+<body>
+        <button id="registrar" onclick="location.href='http://localhost/insertar.php'">Registrar</button>
+
+    <h1 class="queen">QUEEN HOTEL</h1>
+
+<p span style="background-color:aliceblue;margin:auto;display:block;justify-content:justify;align-items:justify;margin-top:70px;margin-left:50px;width:510px;padding: 5px 6px 5px 6px; font-family:sans-serif,arial;height:55px;border-radius: 5px;background-color: rgba(255, 255, 255, 0.3);  backdrop-filter: blur(10px);
+  -webkit-backdrop-filter: blur(10px);" class="texto_1">Bienvenido a, <span style="font-style:italic;" class="queenhotel"> QUEEN HOTEL .</span> Esperamos que disfrute
+de su estancia. Para poder hospedarse y reservar habitaciones primero <br>debe registrarse.</span></p>
+
+</body>
+</html>
